be55c9d625fbab2e946a28726869c66d2c3efea5
Ulises Alexander Arguelles Monjaraz <30351520+UlisesAlexanderAM@users.noreply.github.com>
2023-06-09 19:22:59 -0600

    feat: create a list of movies
    Each movie is a dictionary

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ app = FastAPI()
 app.title = "My application with FastAPI and Platzi"
 app.version = "0.0.1"
 
+movies: list[dict] = [
+    {
+        "id": 1,
+        "title": "Avatar",
+        "overview": "En un exuberante planeta llamado Pandora viven los Na'vi, seres que ...",
+        "year": 2009,
+        "rating": 7.8,
+        "category": "Acción",
+    }
+]
 
 @app.get("/", tags=["home"])
 def message():
