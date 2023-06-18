ee6d8fa57b7b8e7ee96297394ea3dda9bcf3b1f3
Ulises Alexander Arguelles Monjaraz <30351520+UlisesAlexanderAM@users.noreply.github.com>
2023-06-18 14:19:46 -0600

    refactor: avoid conversion from Settings to dict

--- security.py
+++ security.py
@@ -40,7 +40,7 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    enconded_jwt = jwt.encode(to_encode, settings["SECRET_KEY"], settings["ALGORITHM"])
+    enconded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, settings.ALGORITHM)
     return enconded_jwt
 
 
@@ -51,9 +51,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(
-            token, settings["SECRET_KEY"], algorithms=[settings["ALGORITHM"]]
-        )
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=settings.ALGORITHM)
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
